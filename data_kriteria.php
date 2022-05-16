d780e7870a07b3fbaba92dbd97705cebee6e68cd
FarhanMirzah <62887690+FarhanMirzah@users.noreply.github.com>
2022-05-16 17:03:38 +0700

    Perbaiki Title halaman

--- data_kriteria.php
+++ data_kriteria.php
@@ -52,7 +52,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     
-    <title>Sistem Pendukung Keputusan</title>
+    <title>Data Kriteria - SPK WP</title>
   </head>
   
   <body>
